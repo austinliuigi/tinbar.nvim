ab67aa4220001bc3ac2c0bd4cdbd921e4a32a71c
Austin Liu <austinhliu@gmail.com>
2022-12-01 19:15:56 -0800

    fix: Check if hl group exists to avoid error

--- lua/tinbar/utils.lua
+++ lua/tinbar/utils.lua
@@ -5,7 +5,13 @@ local M = {}
 -- @param attribute attribute to target
 -- @return string hex code
 M.get_hl = function (group, attribute)
-  return string.format("#%06x", vim.api.nvim_get_hl_by_name(group, true)[attribute])
+  local hl = vim.api.nvim_get_hl_by_name(group, true)[attribute]
+  if hl ~= nil then
+    return string.format("#%06x", vim.api.nvim_get_hl_by_name(group, true)[attribute])
+  else
+    print(attribute .. " not set for group " .. group)
+    return string.format("#%06x", vim.api.nvim_get_hl_by_name("Statement", true)[attribute])
+  end
 end
 
 return M
