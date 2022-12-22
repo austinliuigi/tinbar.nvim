c1655c77cd68b0b01aa9f441049d82035ec9ddb4
Austin Liu <austinhliu@gmail.com>
2022-12-21 22:25:44 -0800

    Remove components with noice.nvim dependency in config

--- lua/tinbar.lua
+++ lua/tinbar.lua
@@ -32,9 +32,6 @@ end
 tinbar.left_components = function()
   return {
     components.get_left_padding(),
-    components.get_showcmd(),
-    components.get_macro_msg(),
-    components.get_left_padding(),
   }
 end
 
